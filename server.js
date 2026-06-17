916d011a01e8f489ef21cd37b6733996f2c81962
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-06-17 05:38:50 +0500

    now my server will respond to user using res method on route / which is home route

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("First Milestone");
+});
+
 app.listen(3500, () => {
   console.log("Server is running properly...!");
 });
