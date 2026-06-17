0bfc1631aa5d1be80e8eba2c28b27ffe4c3ce636
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-06-17 06:03:09 +0500

    starting to create a notes app to learn REST api

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
 const app = require("./src/app");
 
-app.get("/", (req, res) => {
-  // we can access the data which is coming from frontend using req method
-  res.send("First Milestone");
-});
-
-app.get("/about-page", (req, res) => {
-  res.send("About Page");
-});
-
 app.listen(3500, () => {
-  console.log("Server is running properly...!");
+  console.log("Server is running properly...! on port 3500");
 });
--- src/app.js
+++ src/app.js
@@ -2,4 +2,6 @@ const express = require("express");
 
 const app = express(); // when we call this function we create a instance of express and than store in app variable.
 
+const notes = [];
+
 module.exports = app;
