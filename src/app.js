799a354cbeeb0a031aaf7867dfdae3ec83f016a6
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-06-19 20:39:58 +0500

    added a middleware to get json responses and handle json data

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 
 const app = express(); // when we call this function we create a instance of express and than store in app variable.
+express.json(); // middleware is required to handle json data
 
 const notes = [];
 
 app.post("/notes", (req, res) => {
-  const { title, id, description } = req.body;
+  const { title, description } = req.body;
   console.log(req.body);
   const note = {
-    id: id,
     title: title,
     description: description,
   };
