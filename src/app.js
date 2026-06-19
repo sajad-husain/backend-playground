f486f4c00f0755249db1efd029416aae92613de6
Sajjad Hussain <sajjadhusain8084@gmail.com>
2026-06-19 06:50:02 +0500

    creating post api

--- src/app.js
+++ src/app.js
@@ -4,4 +4,14 @@ const app = express(); // when we call this function we create a instance of exp
 
 const notes = [];
 
+app.post("/notes", (req, res) => {
+  const { title, id, description } = req.body;
+  console.log(req.body);
+  const note = {
+    id: id,
+    title: title,
+    description: description,
+  };
+});
+
 module.exports = app;
